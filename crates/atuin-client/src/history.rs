49103ed05fa3762b5e197077c980992eda4d9d24
JZL <jonahmail1@gmail.com>
2024-05-19 00:24:36 -0400

    Simple don't filter if starts with space

--- crates/atuin-client/src/history.rs
+++ crates/atuin-client/src/history.rs
@@ -377,7 +377,7 @@ impl History {
         let secret_regex = SECRET_PATTERNS.iter().map(|f| f.1);
         let secret_regex = RegexSet::new(secret_regex).expect("Failed to build secrets regex");
 
-        !(self.command.starts_with(' ')
+        !(false
             || settings.history_filter.is_match(&self.command)
             || settings.cwd_filter.is_match(&self.cwd)
             || (secret_regex.is_match(&self.command)) && settings.secrets_filter)
